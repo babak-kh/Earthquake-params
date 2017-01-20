426aa480be71fa5aec299fd9d69369a076b8a8ed
babak-kh <babak.khazaie@gmail.com>
2017-01-20 15:49:55 +0330

    bug fix

--- Codes/Method/plot.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-magnitudes = np.genfromtxt('/home/babak/PycharmProjects/Simulation/GeneralInversion/Inputs/2046-L.txt')
-plt.plot(magnitudes)
-plt.show()
